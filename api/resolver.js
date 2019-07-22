81670545e681c2fc55b440907ae10c403e91fb83
Adalon <etienne.duverney@viacesi.fr>
2019-07-22 14:21:28 +0200

    allCampagnes + allPersonnages

--- api/resolver.js
+++ api/resolver.js
@@ -1,48 +1,68 @@
 const connector = require("./pgConnector")
 const db = connector.db
 
-db.any(`SELECT * FROM joueur`)
-    .then(data => {
-        console.log(data)
-    })
-    .catch(error => {
-        console.log('ERROR:', error);
-    })
-
 const queries = {
     getJoueurs: () => {
         return new Promise(reponse => {
             db.any(`SELECT * FROM joueur`)
-                .then(data => {
-                    console.log(data)
-                    reponse(data)
-                })
-                .catch(error => {
-                    console.log('ERROR:', error);
-                })
-                
-        })
-    },
-    getJeux: () => {
-        return new Promise(response => {
-            db.any(`SELECT * FROM jeu`)
-                .then(data => {
-                    response(data)
-                })
-                .catch(error => {
-                    console.log('ERROR:', error);
-                })
+                .then(data => { reponse(data) })
+                .catch(error => { console.log(error) })
         })
     },
     getJoueur: (idJoueur) => {
-        return new Promise(response => {
-            db.one(`SELECT * FROM joueur WHERE ID=${idJoueur}`)
-                .then(data => {
-                    response(data)
-                })
-                .catch(error => {
-                    console.log('ERROR:', error);
-                })
+        return new Promise(reponse => {
+            db.one(`SELECT * FROM joueur WHERE id=${idJoueur}`)
+                .then(data => { reponse(data) })
+                .catch(error => { console.log(error) })
+        })
+    },
+    getJeux: () => {
+        return new Promise(reponse => {
+            db.any(`SELECT * FROM jeu`)
+                .then(data => { reponse(data) })
+                .catch(error => { console.log(error) })
+        })
+    },
+    getJeu: (idJeu) => {
+        return new Promise(reponse => {
+            db.one(`SELECT * FROM jeu WHERE id=${idJeu}`)
+                .then(data => { reponse(data) })
+                .catch(error => { console.log(error) })
+        })
+    },
+    getCampagnes: (idMj) => {
+        return new Promise(reponse => {
+            db.any(`SELECT * FROM campagne WHERE idMj1=${idMj} OR idMj2=${idMj} OR idMj3=${idMj}`)
+                .then(data => { reponse(data) })
+                .catch(error => { console.log(error) })
+        })
+    },
+    getCampagneScenario: (idCampagne) => {
+        return new Promise(reponse => {
+            db.one(`SELECT * FROM scenario WHERE idCampagne=${idCampagne}`)
+                .then(data => { reponse(data) })
+                .catch(error => { console.log(error) })
+        })
+    },
+    getCampagneLog: (idCampagne) => {
+        return new Promise(reponse => {
+            db.one(`SELECT * FROM log WHERE idCampagne=${idCampagne}`)
+                .then(data => { reponse(data) })
+                .catch(error => { console.log(error) })
+        })
+    },
+    getCampagneNote: (idCampagne) => {
+        return new Promise(reponse => {
+            db.one(`SELECT * FROM note WHERE idCampagne=${idCampagne}`)
+                .then(data => { reponse(data) })
+                .catch(error => { console.log(error) })
+        })
+    },
+    getPersonnage: (idPersonnage) => {
+        return new Promise(reponse => {
+            db.one(`SELECT * FROM personnage WHERE id=${idPersonnage}`)
+                .then(data => { reponse(data) })
+                .catch(error => { console.log(error) })
         })
     },
 }
@@ -51,20 +71,74 @@ const resolvers = {
     Query: {
         allJoueurs: async () => {
             const data = await queries.getJoueurs()
-            console.log(data)
             return data
         },
         allJeux: async () => {
             const data = await queries.getJeux()
-            console.log(data)
+            console.log("infoPerso:", data[0].infopersonnage)
             return data
         },
         joueur: async (root, { id }) => {
             const data = await queries.getJoueur(id)
-            console.log(data)
-            return data[0]
+            return data
         },
-    }
+        jeu: async (root, { id }) => {
+            const data = await queries.getJeu(id)
+            return data
+        },
+        allCampagnes: async (root, { id }) => {
+            const data = await queries.getCampagnes(id)
+            return data
+        },
+        personnageData: async (root, { id }) => {
+            const data = await queries.getPersonnage(id)
+            return data
+        },
+    },
+    Campagne: {
+        mj1: async (parent, args, context, info) => {
+            const data = await queries.getJoueur(parent.idmj1)
+            return data
+        },
+        mj2: async (parent, args, context, info) => {
+            if (parent.idmj2) {
+                const data = await queries.getJoueur(parent.idmj2)
+                return data
+            }
+        },
+        mj3: async (parent, args, context, info) => {
+            if (parent.idmj3) {
+                const data = await queries.getJoueur(parent.idmj3)
+                return data
+            }
+        },
+        jeu: async (parent, args, context, info) => {
+            const data = await queries.getJeu(parent.idjeu)
+            return data
+        },
+        scenario: async (parent, args, context, info) => {
+            const data = await queries.getCampagneScenario(parent.id)
+            return data
+        },
+        log: async (parent, args, context, info) => {
+            const data = await queries.getCampagneLog(parent.id)
+            return data
+        },
+        note: async (parent, args, context, info) => {
+            const data = await queries.getCampagneNote(parent.id)
+            return data
+        }
+    },
+    Personnage: {
+        jeu: async (parent, args, context, info) => {
+            const data = await queries.getJeu(parent.idjeu)
+            return data
+        },
+        joueur: async (parent, args, context, info) => {
+            const data = await queries.getJoueur(parent.idjoueur)
+            return data
+        },
+    },
 }
 
 
--- api/schema.js
+++ api/schema.js
@@ -7,22 +7,56 @@ const typeDefs = gql`
 scalar JSON
 
     type Joueur {
-        id: Int!
+        id: ID!
         nom: String!
         prenom: String!
         mail: String!
     }
-
     type Jeu {
-        id: Int!
+        id: ID!
         nom: String!
         infoPersonnage: JSON
     }
+    type Campagne {
+        id: ID!
+        nom: String!
+        mj1: Joueur!
+        mj2: Joueur
+        mj3: Joueur
+        jeu: Jeu!
+        scenario: Scenario!
+        log: Log!
+        note: Note!
+    }
+    type Scenario {
+        id: ID!
+        content: String
+    }
+    type Log {
+        id: ID!
+        content: JSON
+    }
+    type Note {
+        id: ID!
+        content: String
+    }
+    type Personnage {
+        id: ID!
+        nom: String!
+        info: JSON
+        jeu: Jeu!
+        joueur: Joueur
+    }
+
+
 
     type Query {
-        allJoueurs: [Joueur]
-        allJeux: [Jeu]
-        joueur(id: Int!): Joueur
+        allJoueurs: [Joueur!]
+        allJeux: [Jeu!]
+        allCampagnes(id: ID!): [Campagne!]
+        joueur(id: ID!): Joueur
+        jeu(id: ID!): Jeu
+        personnageData(id: ID!): Personnage
     }
 
 `
