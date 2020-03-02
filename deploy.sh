7a21091415dfd524aa25547a626881c6738ed692
Baptiste Saclier <baptiste.saclier@fr.thalesgroup.com>
2020-03-02 11:50:28 +0100

    Ajout du script de déploiement

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+echo "checking out latest version"
+git pull origin master
+
+VERSION=$(git log --oneline | head -n 1 | cut -d' ' -f1)
+export VERSION
+echo "DEPLOYING version $VERSION"
+
+docker build . -t  etienneduv/graphapi-jdr
+
+echo "Starting stack"
+docker stack deploy --compose-file docker-compose.yml adalon-jdr
+
+echo "Cleaning old containers"
+docker ps | grep "adalon-jdr_" | grep -v "_db" | cut -d' ' -f1 | xargs docker rm -f
